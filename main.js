ca42a62bee08cbef38d9e5f2f5162c8145944cce
honsda <spe6games@gmail.com>
2021-03-31 07:59:45 +0700

    bub

---
+++ main.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js')
+const client = new Discord.Client();
+const classify = require("./gzor.json")
+const chalk = require('chalk');
+const ms = require('ms')
+const moment = require('moment')
+const long = require('long');
+
+const fs = require('fs');
+const { env } = require('process');
+
+client.on('ready', async () =>{
+	require('./events/event_ready.js')(client);
+});
+
+client.on('guildMemberAdd', member => {
+    try {
+        const role = member.guild.roles.cache.find(role => role.id === '810484463237857280');
+        member.roles.add(role)
+    } catch (error) {
+        console.log(error)
+    }
+    const channeljoin = member.guild.channels.cache.find(ch => ch.id === '825915304021721109');
+    const channellog = member.guild.channels.cache.find(ch => ch.id === '826072448994770985');
+    channeljoin.send(`${member} **has joined the server.**`)
+    channellog.send({embed: new Discord.MessageEmbed()
+        .setTitle(`${member.user.tag} joined the server.`)
+        .setDescription(`${member} has joined the server, ID: ${member.id}`)
+        .setColor(0x00C303)
+        .setImage(`${member.user.displayAvatarURL({ format: 'webp', dynamic: true, size: 128 })}`)
+        .setTimestamp()
+    })
+})
+
+client.on('guildMemberRemove', member => {
+    const channellog = member.guild.channels.cache.find(ch => ch.id === '826072448994770985');
+    channellog.send({embed: new Discord.MessageEmbed()
+        .setTitle(`${member.user.tag} left the server.`)
+        .setDescription(`${member} has left the server, ID: ${member.id}`)
+        .setColor(0x00C303)
+        .setImage(`${member.user.displayAvatarURL({ format: 'webp', dynamic: true, size: 128 })}`)
+        .setTimestamp()
+    })
+})
+
+client.login(process.env.YPLRM)
